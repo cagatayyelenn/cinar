56abc5ec1ab1dee3e768a48a2880e0fd3c253773
cagatayyelen <cagatayyelen@gmail.com>
2026-04-02 23:34:54 +0300

    Add TopBar above Header in Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import TopBar from './TopBar';
 import Header from './Header';
 import Footer from './Footer';
 
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col font-sans text-black relative">
+      <TopBar />
       <Header />
       <main className="flex-grow">
         <Outlet />
